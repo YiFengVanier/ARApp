f1fcb96821cca876e26152ecbec2d25a894ad2d0
RanChiVanier <36086032+RanChiVanier@users.noreply.github.com>
2018-04-19 20:48:23 -0400

    Update
    Got every cabinet in the room working in the scan function portion of the application!

--- APPUITEST copy/Assets/SearchDropDown.cs
+++ APPUITEST copy/Assets/SearchDropDown.cs
@@ -7,51 +7,210 @@ public class SearchDropDown : MonoBehaviour {
 	public GameObject Scissor;
 	public GameObject Cup;	
 	public GameObject Fork;	
-	public GameObject Dish;	
+	public GameObject Dish;
+	public GameObject Spoon;	
+	public GameObject Knife;	
+	public GameObject Pen;	
+	public GameObject Cardboard;	
+	public GameObject Tissue;	
+	public GameObject Book;	
+	public GameObject Keyboard;	
 
 	public Dropdown dropdown;
 	public Text text;
 
 	void Update () {
 
+		//Shows all containers
 		if (dropdown.value == 0) {
 			text.text = "Scanning for All";
 			Scissor.SetActive (true);
 			Cup.SetActive (true);
 			Fork.SetActive (true);
 			Dish.SetActive (true);
+			Spoon.SetActive (true);
+			Knife.SetActive (true);
+			Pen.SetActive (true);
+			Cardboard.SetActive (true);
+			Tissue.SetActive (true);
+			Book.SetActive (true);
+			Keyboard.SetActive (true);	
 		}
 
+		//Shows only Dish container
 		if (dropdown.value == 1) {
 			text.text = "Scanning for Dishes";
 			Scissor.SetActive (false);
 			Cup.SetActive (false);
 			Fork.SetActive (false);
 			Dish.SetActive (true);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
 		}
 
+		//Shows only Fork container
 		if (dropdown.value == 2) {
 			text.text = "Scanning for Forks";
 			Scissor.SetActive (false);
 			Cup.SetActive (false);
 			Fork.SetActive (true);
 			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
 		}
 
+		//Shows only Cup container
 		if (dropdown.value == 3) {
 			text.text = "Scanning for Cups";
 			Scissor.SetActive (false);
 			Cup.SetActive (true);
 			Fork.SetActive (false);
 			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);		
 		}
 
+		//Shows only Scissor container
 		if (dropdown.value == 4) {
 			text.text = "Scanning for Scissors";
 			Scissor.SetActive (true);
 			Cup.SetActive (false);
 			Fork.SetActive (false);
 			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
+		}
+
+		//Shows only Spoon container
+		if (dropdown.value == 5) {
+			text.text = "Scanning for Spoons";
+			Scissor.SetActive (false);
+			Cup.SetActive (false);
+			Fork.SetActive (false);
+			Dish.SetActive (false);
+			Spoon.SetActive (true);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
+		}
+
+		//Shows only Knife container
+		if (dropdown.value == 6) {
+			text.text = "Scanning for Knives";
+			Scissor.SetActive (false);
+			Cup.SetActive (false);
+			Fork.SetActive (false);
+			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (true);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
+		}
+
+		//Shows only Pen container
+		if (dropdown.value == 7) {
+			text.text = "Scanning for Pens";
+			Scissor.SetActive (false);
+			Cup.SetActive (false);
+			Fork.SetActive (false);
+			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (true);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
+		}
+
+		//Shows only Cardboard container
+		if (dropdown.value == 8) {
+			text.text = "Scanning for Cardboard";
+			Scissor.SetActive (false);
+			Cup.SetActive (false);
+			Fork.SetActive (false);
+			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (true);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
+		}
+
+		//Shows only Tissue container
+		if (dropdown.value == 9) {
+			text.text = "Scanning for Tissues";
+			Scissor.SetActive (false);
+			Cup.SetActive (false);
+			Fork.SetActive (false);
+			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (true);
+			Book.SetActive (false);
+			Keyboard.SetActive (false);	
+		}
+
+		//Shows only Book container
+		if (dropdown.value == 10) {
+			text.text = "Scanning for Books";
+			Scissor.SetActive (false);
+			Cup.SetActive (false);
+			Fork.SetActive (false);
+			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (true);
+			Keyboard.SetActive (false);	
+		}
+
+		//Shows only Keyboard container
+		if (dropdown.value == 11) {
+			text.text = "Scanning for Keyboards";
+			Scissor.SetActive (false);
+			Cup.SetActive (false);
+			Fork.SetActive (false);
+			Dish.SetActive (false);
+			Spoon.SetActive (false);
+			Knife.SetActive (false);
+			Pen.SetActive (false);
+			Cardboard.SetActive (false);
+			Tissue.SetActive (false);
+			Book.SetActive (false);
+			Keyboard.SetActive (true);	
 		}
 
 	}
