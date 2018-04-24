c1ac2a270fc5cba4e7859ee1bd7f672b427a50a0
RanChiVanier <36086032+RanChiVanier@users.noreply.github.com>
2018-04-24 14:35:23 -0400

    Update
    Flashlight toggle

---
+++ APPUITEST copy/Assets/Flashlight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Flashlight : MonoBehaviour {
+
+	public void turnon (int toggle) {
+		if (toggle == 1) {
+			CameraDevice.Instance.SetFlashTorchMode (false);
+		}
+		if (toggle == 0) {
+			CameraDevice.Instance.SetFlashTorchMode (true);
+		}
+	}
+}
