a7db0c2bc95d1a3a77107257abf4725657761648
RanChiVanier <36086032+RanChiVanier@users.noreply.github.com>
2018-05-07 12:11:55 -0400

    Update
    HOPEFULLY ARROWS WORK!!! :^)

--- APPUITEST copy/Assets/position.cs
+++ APPUITEST copy/Assets/position.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class position : MonoBehaviour
 {
     public TextMesh Status_Text;
+	public Dropdown dropdown;
 
     // Use this for initialization
     void Start()
@@ -22,8 +24,31 @@ public class position : MonoBehaviour
 
         var xangle = xrot * (180 / Mathf.PI) + 90;
         var zangle = -zrot * (180 / Mathf.PI);
-        transform.eulerAngles = new Vector3((xangle - (xangle%1)), 0, ((zangle - Input.compass.trueHeading) - ((zangle - Input.compass.trueHeading)%1)));
-       
+			
+			//no arrows for all
+			if (dropdown.value == 0) 
+		{
+		}
+			//arrow for 4 cabinet
+			if ((dropdown.value == 1) || (dropdown.value == 2) || (dropdown.value == 3) || (dropdown.value == 4)) 
+		{
+			transform.eulerAngles = new Vector3 ((xangle - (xangle % 1)), 0, ((zangle - Input.compass.trueHeading) - ((zangle - Input.compass.trueHeading) % 1)) - 50);
+		}
+			//arrow for 3 desk
+			if ((dropdown.value == 5) || (dropdown.value == 6) || (dropdown.value == 7)) 
+		{
+			transform.eulerAngles = new Vector3 ((xangle - (xangle % 1)), 0, ((zangle - Input.compass.trueHeading) - ((zangle - Input.compass.trueHeading) % 1)) - 260);
+		}
+			//arrow for right wood cabinet
+			if ((dropdown.value == 8) || (dropdown.value == 9)) 
+		{
+			transform.eulerAngles = new Vector3 ((xangle - (xangle % 1)), 0, ((zangle - Input.compass.trueHeading) - ((zangle - Input.compass.trueHeading) % 1)) - 170);
+		}
+			//arrow for left wood cabinet
+			if ((dropdown.value == 10) || (dropdown.value == 11)) 
+		{
+			transform.eulerAngles = new Vector3 ((xangle - (xangle % 1)), 0, ((zangle - Input.compass.trueHeading) - ((zangle - Input.compass.trueHeading) % 1)) - 70);
+		}
             if (Status_Text != null)
         {
             Status_Text.text = (Input.compass.trueHeading - ((Input.compass.trueHeading)%1)).ToString();
