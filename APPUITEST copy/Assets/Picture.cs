d2d1869a64b80ea5cc2ea9d76376f403cc178969
RanChiVanier <36086032+RanChiVanier@users.noreply.github.com>
2018-05-14 14:34:47 -0400

    UPDATE
    SCREENSHOT works :^)

---
+++ APPUITEST copy/Assets/Picture.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Vuforia;
+
+public class Picture : MonoBehaviour {
+
+	public void Screenshot() {
+		ScreenCapture.CaptureScreenshot ("screenshot.png");
+	}
+}
