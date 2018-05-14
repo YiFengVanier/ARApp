7f7012c67e0d192e3bbe265d46c21bcb374a0c17
RanChiVanier <36086032+RanChiVanier@users.noreply.github.com>
2018-05-14 15:00:34 -0400

    Update!
    Attempting to have clean screenshot

--- APPUITEST copy/Assets/Picture.cs
+++ APPUITEST copy/Assets/Picture.cs
@@ -6,8 +6,15 @@ using UnityEngine.EventSystems;
 using Vuforia;
 
 public class Picture : MonoBehaviour {
+	public GameObject ScreenshotButton;
+	public GameObject BackButton;
 
 	public void Screenshot() {
+		ScreenshotButton.SetActive (false);
+		BackButton.SetActive(false);
 		ScreenCapture.CaptureScreenshot ("screenshot.png");
+		print (Application.persistentDataPath);
+		ScreenshotButton.SetActive (true);
+		BackButton.SetActive(true);
 	}
 }
